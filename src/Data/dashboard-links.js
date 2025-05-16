b527c48f6a365075874b48b3637c16a889d11f49
Dhvani Parghi <dhvaniparghi2003@gmail.com>
2025-05-16 15:37:24 +0530

    Add files via upload

---
+++ src/Data/dashboard-links.js
@@ -0,0 +1,15 @@
+
+export const sidebarLinks = [
+  {
+    id: 1,
+    name: "My Profile",
+    path: "/dashboard/my-profile",
+    icon: "VscAccount",
+  },
+    {
+      id: 2,
+      name: "My jobs",
+      path: "/dashboard/my-jobs",
+      icon: "VscVm",
+    },
+]
---
+++ src/Data/navbar-links.js
@@ -0,0 +1,27 @@
+export const NavbarLinks = [
+  {
+    title: "Home",
+    path: "/",
+  },
+  {
+    title: "About Us",
+    path: "/about",
+  },
+  {
+    title: "Find Jobs",
+    path: "/job",
+  },
+  {
+    title: "Blog",
+   path:"/blog",
+  },
+ 
+  {
+    title: "Contact Us",
+    path: "/contact",
+  },
+  {
+    title: "Make Resume",
+    path: "/MakeResume"
+  },
+];
